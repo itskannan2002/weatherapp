7532eed7d10f8b2d4c4e4247a5c0735936d29cb3
itskannan2002 <thalavk008@gmail.com>
2022-02-19 11:08:41 +0530

    commit

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'hz42qp&5^*fr_s-k-)w@p*uewozvu5_j9!4-&-%1rf)&dh1!f8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
