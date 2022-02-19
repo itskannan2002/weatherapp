10795c02d902dfd73971b8c67791bde2e225c39f
itskannan2002 <thalavk008@gmail.com>
2022-02-19 10:54:22 +0530

    Files upload

---
+++ weatherapp/views.py
@@ -0,0 +1,28 @@
+import urllib.request
+import json
+from django.shortcuts import render
+
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=db6637b839f63e2d7e7aa5c9d2d31874').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', ' + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "main/index.html", data)
